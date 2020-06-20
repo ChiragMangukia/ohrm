27d9b4fd714666665c56a40dc4bf18b398eca8c1
Chirag Mangukia <chirag.mangukia@gmail.com>
2020-06-20 15:30:10 +0530

    Create Maven build

---
+++ src/main/java/com/chirag/seljava/ohrm/App.java
@@ -0,0 +1,13 @@
+package com.chirag.seljava.ohrm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
